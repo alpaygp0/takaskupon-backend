bb1d4ddfff43e00be60bad0a377dfa0bd8dca663
Alpay Garip <022340570@ogr.uludag.edu.tr>
2026-03-31 11:56:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use((err, req, res, next) => {
 // ==========================================
 // 8. VERİTABANI VE BAŞLATMA
 // ==========================================
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Sunucu ${PORT} portunda aktif!`);
